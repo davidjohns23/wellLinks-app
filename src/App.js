0f907e236ecc823f6fb2b5fcc6bbea7c08fee086
David Johns <david.johns@student.fairfield.edu>
2023-04-24 11:23:46 -0400

    please work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/index";
-import StateList from "./pages/StatesList";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
-        <Route path="/states" element={<StateList />} />
+        
       </Routes>
     </BrowserRouter>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,46 +3,41 @@ import axios from "axios";
 
 
 const Index = () => {
-  // Define state variables
   const [countries, setCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState(null);
   const [selectedCountryStates, setSelectedCountryStates] = useState([]);
 
-  // Fetch country data from API using useEffect hook
   useEffect(() => {
     axios
       .get("https://countriesnow.space/api/v0.1/countries/info?returns=currency,unicodeFlag,dialCode")
       .then((response) => {
-        setCountries(response.data.data.slice(8, 18)); // Store the data for 10 countries in the state variable
+        setCountries(response.data.data.slice(8, 18));
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  // Event handler for when a country is clicked
   const handleCountryClick = (country) => {
-    setSelectedCountry(country); // Set the selected country in state
-    setSelectedCountryStates([]); // Clear any existing state data
+    setSelectedCountry(country);
+    setSelectedCountryStates([]);
   };
 
-  // Event handler for when a country flag is clicked
   const handleFlagClick = () => {
     axios
       .get(`https://countriesnow.space/api/v0.1/countries/states?country=${selectedCountry.name}`)
       .then((response) => {
         const countryData = response.data.data.find((country) => country.name === selectedCountry.name);
-        setSelectedCountryStates(countryData.states.slice(0, 10)); // Store the data for the first 10 states in the selected country
+        setSelectedCountryStates(countryData.states.slice(0, 10));
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  // Event handler for when the Back button is clicked
   const handleBackClick = () => {
-    setSelectedCountry(null); // Clear the selected country from state
-    setSelectedCountryStates([]); // Clear any existing state data
+    setSelectedCountry(null);
+    setSelectedCountryStates([]);
   };
 
   return (
@@ -95,7 +90,6 @@ const Index = () => {
 };
 
 export default Index;
-
 
 
 
